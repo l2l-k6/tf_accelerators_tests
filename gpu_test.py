5abe6cccaf949dab6c5d811349e95a46ebe77d99
L2L K6 <k6@n16a9.de>
2024-12-17 18:42:26 +0100

    Minor changes in the GPU test.

--- gpu_test.py
+++ gpu_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-print("Interpeter loaded successfully.")
-
 import tensorflow as tf
-print("Number of GPUs available: ", len(tf.config.list_physical_devices("GPU")))
+
+
+if __name__ == '__main__':
+	print("Number of GPUs available: ", len(tf.config.list_physical_devices("GPU")))
